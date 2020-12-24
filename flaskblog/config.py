a8f866fd05d5be87df43bf00877ef7d29005fce0
sarayu-suresh <sarayusuresh88@gmail.com>
2020-12-24 10:52:10 +0530

    Updated

--- flaskblog/config.py
+++ flaskblog/config.py
@@ -1,7 +1,7 @@
 # Needed setting secret key, email and psswd
 from os import environ
 class Config:
-    SECRET_KEY = environ.get('SECRET')
+    SECRET_KEY = 'qwertyuiop'
     SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL') or 'sqlite:///site.db'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
